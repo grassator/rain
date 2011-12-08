7a3ef72cf27335cd92eed2f0a4bfae864b1a0315
Dmitriy Kubyshkin <dmitriy@kubyshkin.ru>
2011-12-08 13:16:01 +0700

    Added functions to save 3-part and 9-part images.

--- main.cpp
+++ main.cpp
@@ -11,6 +11,11 @@
 #include "lodepng.h"
 #include "guidelines.h"
 
+enum direction {
+  HORIZONTAL = 0,
+  VERTICAL
+};
+
 void saveImage(std::vector<unsigned char> &image,
                unsigned width, unsigned height,
                std::string filename)
@@ -59,6 +64,85 @@ void saveMinimized(rain::guidelines guides, std::vector<unsigned char> &image,
   saveImage(minimized, outputWidth, outputHeight, filename);
 }
 
+// Saves all 9 parts of an image for resizing in both directions
+void saveNinePart(rain::guidelines guides, std::vector<unsigned char> &image,
+                  unsigned width, unsigned height, std::string basename)
+{
+  const char * names[3][3] = {
+    { "north-west",  "north",  "north-east" },
+    { "west",        "middle", "east"       },
+    { "south-west",  "south",  "south-east" }
+  };
+  unsigned sizeX[3] = { guides.left,  1,            guides.right           };
+  unsigned sizeY[3] = { guides.top,   1,            guides.bottom          };
+  unsigned fromX[3] = { 0,            guides.left,  width - guides.right   };
+  unsigned fromY[3] = { 0,            guides.top,   height - guides.bottom };
+
+  for(char x = 0; x < 3; ++x)
+  {
+    for(char y = 0; y < 3; ++y)
+    {
+      std::vector<unsigned char> output;
+      output.resize(sizeX[x] * sizeY[y] * 4);
+      rain::copyImagePart(
+        image, width,
+        output, sizeX[x],
+        sizeX[x], sizeY[y],
+        fromX[x], fromY[y],
+        0, 0
+      );
+
+      saveImage(output, sizeX[x], sizeY[y], basename + "-" + names[y][x] + ".png");
+    }
+  }
+}
+
+
+// Saves three images either in horizontal or vertical direction
+void saveThreePart(rain::guidelines guides, std::vector<unsigned char> &image,
+                   unsigned width, unsigned height, std::string basename,
+                   direction dir)
+{
+  if(dir == HORIZONTAL)
+  {
+    const char * names[3] = { "left",  "middle", "right" };
+    unsigned sizeX[3] = { guides.left,  1,            guides.right           };
+    unsigned fromX[3] = { 0,            guides.left,  width - guides.right   };
+    
+    for(char x = 0; x < 3; ++x)
+    {
+      std::vector<unsigned char> output;
+      output.resize(sizeX[x] * height * 4);
+      rain::copyImagePart(
+        image, width,
+        output, sizeX[x],
+        sizeX[x], height,
+        fromX[x], 0, 0, 0
+      );
+      saveImage(output, sizeX[x], height, basename + "-" + names[x] + ".png");
+    }
+  }
+  else if(dir == VERTICAL)
+  {
+    const char * names[3] = { "top",  "middle", "bottom" };
+    unsigned sizeY[3] = { guides.top,   1,            guides.bottom          };
+    unsigned fromY[3] = { 0,            guides.top,   height - guides.bottom };
+    
+    for(char y = 0; y < 3; ++y)
+    {
+      std::vector<unsigned char> output;
+      output.resize(width * sizeY[y] * 4);
+      rain::copyImagePart(
+        image, width,
+        output, width,
+        width, sizeY[y],
+        0, fromY[y], 0, 0
+      );
+      saveImage(output, width, sizeY[y], basename + "-" + names[y] + ".png");
+    }
+  }
+}
+
 
 
 int main (int argc, const char * argv[])
@@ -68,7 +152,7 @@ int main (int argc, const char * argv[])
   std::vector<unsigned char> buffer, image;
   LodePNG::loadFile(buffer, filename); //load the image file with given filename
   LodePNG::Decoder decoder;
-  decoder.decode(image, buffer.empty() ? 0 : &buffer[0], (unsigned)buffer.size()); //decode the png
+  decoder.decode(image, buffer.empty() ? 0 : &buffer[0], (unsigned)buffer.size());
   
   //if there's an error, display it, otherwise display information about the image
   if(decoder.hasError())
@@ -87,7 +171,9 @@ int main (int argc, const char * argv[])
     "left: " << result.left << "\n" <<
     "bottom: " << result.bottom << "\n";
 
-    saveMinimized(result, image, decoder.getWidth(), decoder.getHeight(), "min.png");
+    saveNinePart(
+      result, image, decoder.getWidth(), decoder.getHeight(), "nine-part"
+    );
   }
 }
     // std::cout << "\n" <<
